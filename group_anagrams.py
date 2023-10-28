d8d10fddd690c6c881e4a2a203e662a1c05755bb
Pranamika Pandey <47104271+Pihu1998@users.noreply.github.com>
2023-10-28 19:46:49 +0100

    Update group_anagrams.py

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,12 +1,17 @@
 '''
 Leetcode question: https://leetcode.com/problems/group-anagrams/
 Level: Medium
+Solution: Hash map
 Time complexity: O(n)
 Space complexity: O(n)
 '''
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        Maintain a hashmap with key of type string and value of type "list of strings" so that 
+        we can have multiple values for a particular key
+        '''        
         seen ={}
         for word in strs:
             sorted_word = ''.join(sorted(word))
