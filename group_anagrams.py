7e4fc9d970974cc0cf8139470f771552c5f660d4
Pranamika Pandey <47104271+Pihu1998@users.noreply.github.com>
2023-10-28 19:44:04 +0100

    Create group_anagrams.py

---
+++ group_anagrams.py
@@ -0,0 +1,18 @@
+'''
+Leetcode question:
+Level: Medium
+Time complexity: O(n)
+Space complexity: O(n)
+'''
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen ={}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in seen:
+                seen[sorted_word].append(word)
+            else:
+                seen[sorted_word] = [word]    
+
+        return seen.values()        
